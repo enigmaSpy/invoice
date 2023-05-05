cdf960c62e6524f7b038a6c167e263d135a83592
Kamil Soczewica <soczekv18@gmail.com>
2023-05-05 20:59:58 +0200

    update sum section

--- src/SummSection/index.js
+++ src/SummSection/index.js
@@ -1,13 +1,31 @@
+import { useState, useEffect } from "react";
+import { MainSection } from "./styled";
 
+const SummSection = ({ services }) => {
 
-const SummSection = () => {
-    return (
-        <div className="summ-section">
-            łacznie brutto: 123 zł
-            <br/>
-            łacznie netto: 100 zł
-            
-        </div>
+    const [totalBrutto, setTotalBrutto] = useState(0);
+    const [totalNetto, setTotalNetto] = useState(0);
+    const [totalDiscount, setTotalDiscount] = useState(0);
+
+    const countTotal = () => {
+        const newTotalBrutto = services.reduce((prevValue, {brutto}) => prevValue + brutto, 0);
+        const newTotalNetto = services.reduce((prevValue, {netto}) => prevValue + netto, 0);
+        const newTotalDiscount = services.reduce((prevValue, {discountPrice})=> prevValue + discountPrice, 0);
+        setTotalBrutto(newTotalBrutto.toFixed(2));
+        setTotalNetto(newTotalNetto.toFixed(2));
+        setTotalDiscount(newTotalDiscount.toFixed(2));
+    };
+
+    useEffect(() => {
+        countTotal();
+    }, [services]);
+
+    return services.length > 0 && (
+      (  <MainSection>
+            <p>Łacznie netto: {totalNetto} zł</p>
+            <p>Łacznie brutto: {totalBrutto} zł</p>
+            <p>Łączna wartość rabatu:{totalDiscount}zł</p>
+        </MainSection>)
     );
 }
 
